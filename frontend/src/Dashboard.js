4057a45596b95fc8db0e9dab8f63b055d700c04d
Darryl Soh Soon Yong <ssy.darryl@gmail.com>
2022-08-27 19:21:32 +0800

    Setup dashboard board

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -13,20 +13,10 @@ export default class Dashboard extends Component {
     super();
     this.state = {
       token: '',
-      openProductModal: false,
-      openProductEditModal: false,
-      id: '',
-      name: '',
-      desc: '',
-      price: '',
-      discount: '',
-      file: '',
-      fileName: '',
-      page: 1,
-      search: '',
-      products: [],
-      pages: 0,
-      loading: false
+      income: '',
+      questionnaireScore: '',
+      wealth: '',
+      registrationDate: '',
     };
   }
 
@@ -36,69 +26,36 @@ export default class Dashboard extends Component {
       this.props.history.push('/login');
     } else {
       this.setState({ token: token }, () => {
-        this.getProduct();
+        this.getUser();
       });
     }
   }
 
-  getProduct = () => {
+  getUser = () => {
     
     this.setState({ loading: true });
 
     let data = '?';
-    data = `${data}page=${this.state.page}`;
-    if (this.state.search) {
-      data = `${data}&search=${this.state.search}`;
-    }
-    axios.get(`http://localhost:2000/get-product${data}`, {
+    data = `${data}`;
+    axios.get(`http://localhost:2000/get-user`, {
+      username: this.state.username,
       headers: {
         'token': this.state.token
       }
+
     }).then((res) => {
-      this.setState({ loading: false, products: res.data.products, pages: res.data.pages });
+      this.setState({ loading: false, wealth: res.data.userdata[0].wealth, income: res.data.userdata[0].income, questionnaireScore: res.data.userdata[0].questionnaireScore, registrationDate: res.data.userdata[0].registrationDate});
+      this.setState({wealth: res.data.userdata[0].wealth})
     }).catch((err) => {
       swal({
         text: err.response.data.errorMessage,
         icon: "error",
         type: "error"
       });
-      this.setState({ loading: false, products: [], pages: 0 },()=>{});
+      this.setState({ loading: false, products: []},()=>{});
     });
   }
 
-  deleteProduct = (id) => {
-    axios.post('http://localhost:2000/delete-product', {
-      id: id
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'token': this.state.token
-      }
-    }).then((res) => {
-
-      swal({
-        text: res.data.title,
-        icon: "success",
-        type: "success"
-      });
-
-      this.setState({ page: 1 }, () => {
-        this.pageChange(null, 1);
-      });
-    }).catch((err) => {
-      swal({
-        text: err.response.data.errorMessage,
-        icon: "error",
-        type: "error"
-      });
-    });
-  }
-
-  pageChange = (e, page) => {
-    this.setState({ page: page }, () => {
-      this.getProduct();
-    });
-  }
 
   logOut = () => {
     localStorage.setItem('token', null);
@@ -112,117 +69,19 @@ export default class Dashboard extends Component {
     this.setState({ [e.target.name]: e.target.value }, () => { });
     if (e.target.name == 'search') {
       this.setState({ page: 1 }, () => {
-        this.getProduct();
+        this.getUser();
       });
     }
   };
-
-  addProduct = () => {
-    const fileInput = document.querySelector("#fileInput");
-    const file = new FormData();
-    file.append('file', fileInput.files[0]);
-    file.append('name', this.state.name);
-    file.append('desc', this.state.desc);
-    file.append('discount', this.state.discount);
-    file.append('price', this.state.price);
-
-    axios.post('http://localhost:2000/add-product', file, {
-      headers: {
-        'content-type': 'multipart/form-data',
-        'token': this.state.token
+  getMonthDifference = (startDate, endDate) => {
+      let months = endDate.getMonth() - startDate.getMonth() + 12 * (endDate.getFullYear() - startDate.getFullYear())
+      if (months > 9){
+        months = 10
       }
-    }).then((res) => {
-
-      swal({
-        text: res.data.title,
-        icon: "success",
-        type: "success"
-      });
-
-      this.handleProductClose();
-      this.setState({ name: '', desc: '', discount: '', price: '', file: null, page: 1 }, () => {
-        this.getProduct();
-      });
-    }).catch((err) => {
-      swal({
-        text: err.response.data.errorMessage,
-        icon: "error",
-        type: "error"
-      });
-      this.handleProductClose();
-    });
-
+      return months;
   }
 
-  updateProduct = () => {
-    const fileInput = document.querySelector("#fileInput");
-    const file = new FormData();
-    file.append('id', this.state.id);
-    file.append('file', fileInput.files[0]);
-    file.append('name', this.state.name);
-    file.append('desc', this.state.desc);
-    file.append('discount', this.state.discount);
-    file.append('price', this.state.price);
 
-    axios.post('http://localhost:2000/update-product', file, {
-      headers: {
-        'content-type': 'multipart/form-data',
-        'token': this.state.token
-      }
-    }).then((res) => {
-
-      swal({
-        text: res.data.title,
-        icon: "success",
-        type: "success"
-      });
-
-      this.handleProductEditClose();
-      this.setState({ name: '', desc: '', discount: '', price: '', file: null }, () => {
-        this.getProduct();
-      });
-    }).catch((err) => {
-      swal({
-        text: err.response.data.errorMessage,
-        icon: "error",
-        type: "error"
-      });
-      this.handleProductEditClose();
-    });
-
-  }
-
-  handleProductOpen = () => {
-    this.setState({
-      openProductModal: true,
-      id: '',
-      name: '',
-      desc: '',
-      price: '',
-      discount: '',
-      fileName: ''
-    });
-  };
-
-  handleProductClose = () => {
-    this.setState({ openProductModal: false });
-  };
-
-  handleProductEditOpen = (data) => {
-    this.setState({
-      openProductEditModal: true,
-      id: data._id,
-      name: data.name,
-      desc: data.desc,
-      price: data.price,
-      discount: data.discount,
-      fileName: data.image
-    });
-  };
-
-  handleProductEditClose = () => {
-    this.setState({ openProductEditModal: false });
-  };
 
   render() {
     return (
@@ -230,15 +89,11 @@ export default class Dashboard extends Component {
         {this.state.loading && <LinearProgress size={40} />}
         <div>
           <h2>Dashboard</h2>
-          <Button
-            className="button_style"
-            variant="contained"
-            color="primary"
-            size="small"
-            onClick={this.handleProductOpen}
-          >
-            Add Product
-          </Button>
+          Income Score: {this.state.income*5}/25 <br/>
+          Questionnaire Score: {this.state.questionnaireScore*2}/20 <br/>
+          Wealth Score: {this.state.wealth*5}/25 <br/>
+          Duration Score: {this.getMonthDifference(new Date(this.state.registrationDate), new Date())}/10 <br/>
+          <h1>Your risk level is {100-(this.state.income*5+this.state.questionnaireScore*2+this.state.wealth*5+this.getMonthDifference(new Date(this.state.registrationDate), new Date()))}%</h1>
           <Button
             className="button_style"
             variant="contained"
@@ -248,233 +103,6 @@ export default class Dashboard extends Component {
             Log Out
           </Button>
         </div>
-
-        {/* Edit Product */}
-        <Dialog
-          open={this.state.openProductEditModal}
-          onClose={this.handleProductClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">Edit Product</DialogTitle>
-          <DialogContent>
-            <TextField
-              id="standard-basic"
-              type="text"
-              autoComplete="off"
-              name="name"
-              value={this.state.name}
-              onChange={this.onChange}
-              placeholder="Product Name"
-              required
-            /><br />
-            <TextField
-              id="standard-basic"
-              type="text"
-              autoComplete="off"
-              name="desc"
-              value={this.state.desc}
-              onChange={this.onChange}
-              placeholder="Description"
-              required
-            /><br />
-            <TextField
-              id="standard-basic"
-              type="number"
-              autoComplete="off"
-              name="price"
-              value={this.state.price}
-              onChange={this.onChange}
-              placeholder="Price"
-              required
-            /><br />
-            <TextField
-              id="standard-basic"
-              type="number"
-              autoComplete="off"
-              name="discount"
-              value={this.state.discount}
-              onChange={this.onChange}
-              placeholder="Discount"
-              required
-            /><br /><br />
-            <Button
-              variant="contained"
-              component="label"
-            > Upload
-            <input
-                id="standard-basic"
-                type="file"
-                accept="image/*"
-                name="file"
-                value={this.state.file}
-                onChange={this.onChange}
-                id="fileInput"
-                placeholder="File"
-                hidden
-              />
-            </Button>&nbsp;
-            {this.state.fileName}
-          </DialogContent>
-
-          <DialogActions>
-            <Button onClick={this.handleProductEditClose} color="primary">
-              Cancel
-            </Button>
-            <Button
-              disabled={this.state.name == '' || this.state.desc == '' || this.state.discount == '' || this.state.price == ''}
-              onClick={(e) => this.updateProduct()} color="primary" autoFocus>
-              Edit Product
-            </Button>
-          </DialogActions>
-        </Dialog>
-
-        {/* Add Product */}
-        <Dialog
-          open={this.state.openProductModal}
-          onClose={this.handleProductClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">Add Product</DialogTitle>
-          <DialogContent>
-            <TextField
-              id="standard-basic"
-              type="text"
-              autoComplete="off"
-              name="name"
-              value={this.state.name}
-              onChange={this.onChange}
-              placeholder="Product Name"
-              required
-            /><br />
-            <TextField
-              id="standard-basic"
-              type="text"
-              autoComplete="off"
-              name="desc"
-              value={this.state.desc}
-              onChange={this.onChange}
-              placeholder="Description"
-              required
-            /><br />
-            <TextField
-              id="standard-basic"
-              type="number"
-              autoComplete="off"
-              name="price"
-              value={this.state.price}
-              onChange={this.onChange}
-              placeholder="Price"
-              required
-            /><br />
-            <TextField
-              id="standard-basic"
-              type="number"
-              autoComplete="off"
-              name="discount"
-              value={this.state.discount}
-              onChange={this.onChange}
-              placeholder="Discount"
-              required
-            /><br /><br />
-            <Button
-              variant="contained"
-              component="label"
-            > Upload
-            <input
-                id="standard-basic"
-                type="file"
-                accept="image/*"
-                // inputProps={{
-                //   accept: "image/*"
-                // }}
-                name="file"
-                value={this.state.file}
-                onChange={this.onChange}
-                id="fileInput"
-                placeholder="File"
-                hidden
-                required
-              />
-            </Button>&nbsp;
-            {this.state.fileName}
-          </DialogContent>
-
-          <DialogActions>
-            <Button onClick={this.handleProductClose} color="primary">
-              Cancel
-            </Button>
-            <Button
-              disabled={this.state.name == '' || this.state.desc == '' || this.state.discount == '' || this.state.price == '' || this.state.file == null}
-              onClick={(e) => this.addProduct()} color="primary" autoFocus>
-              Add Product
-            </Button>
-          </DialogActions>
-        </Dialog>
-
-        <br />
-
-        <TableContainer>
-          <TextField
-            id="standard-basic"
-            type="search"
-            autoComplete="off"
-            name="search"
-            value={this.state.search}
-            onChange={this.onChange}
-            placeholder="Search by product name"
-            required
-          />
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="center">Name</TableCell>
-                <TableCell align="center">Image</TableCell>
-                <TableCell align="center">Description</TableCell>
-                <TableCell align="center">Price</TableCell>
-                <TableCell align="center">Discount</TableCell>
-                <TableCell align="center">Action</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {this.state.products.map((row) => (
-                <TableRow key={row.name}>
-                  <TableCell align="center" component="th" scope="row">
-                    {row.name}
-                  </TableCell>
-                  <TableCell align="center"><img src={`http://localhost:2000/${row.image}`} width="70" height="70" /></TableCell>
-                  <TableCell align="center">{row.desc}</TableCell>
-                  <TableCell align="center">{row.price}</TableCell>
-                  <TableCell align="center">{row.discount}</TableCell>
-                  <TableCell align="center">
-                    <Button
-                      className="button_style"
-                      variant="outlined"
-                      color="primary"
-                      size="small"
-                      onClick={(e) => this.handleProductEditOpen(row)}
-                    >
-                      Edit
-                  </Button>
-                    <Button
-                      className="button_style"
-                      variant="outlined"
-                      color="secondary"
-                      size="small"
-                      onClick={(e) => this.deleteProduct(row._id)}
-                    >
-                      Delete
-                  </Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-          <br />
-          <Pagination count={this.state.pages} page={this.state.page} onChange={this.pageChange} color="primary" />
-        </TableContainer>
-
       </div>
     );
   }
