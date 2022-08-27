d59b650ffb4ad1a277aa3b6ef50f300b6d7a4518
Darryl Soh Soon Yong <ssy.darryl@gmail.com>
2022-08-27 17:35:46 +0800

    Fix issue with registration

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -59,12 +59,72 @@ export default class Register extends React.Component {
             required
           />
           <br /><br />
+          <TextField
+            id="standard-basic"
+            type="password"
+            autoComplete="off"
+            name="password"
+            value={this.state.password}
+            onChange={this.onChange}
+            placeholder="Password"
+            required
+          />
+          <br /><br />
+          <TextField
+            id="standard-basic"
+            type="password"
+            autoComplete="off"
+            name="confirm_password"
+            value={this.state.confirm_password}
+            onChange={this.onChange}
+            placeholder="Confirm Password"
+            required
+          />
+          <br /><br />
+          <FormControl variant="standard" style={{minWidth: 182, textAlign: "left"}}>
+            <InputLabel id="select-basic" >Total Net Worth</InputLabel>
+            <Select
+                id="select-basic"
+                type="wealth"
+                name="wealth"
+                value={this.state.wealth}
+                onChange={this.onChange}
+                required
+            >
+              <MenuItem value={0}>Less than $1000</MenuItem>
+              <MenuItem value={1}>$1000 to $11999</MenuItem>
+              <MenuItem value={2}>$12000 to $35999</MenuItem>
+              <MenuItem value={3}>$36000 to $59999</MenuItem>
+              <MenuItem value={4}>$60000 and $83999</MenuItem>
+              <MenuItem value={5}>$84000 and above</MenuItem>
+            </Select>
+          </FormControl>
+          <br /><br />
+          <FormControl variant="standard" style={{minWidth: 182, textAlign: "left"}}>
+            <InputLabel id="select-basic" >Annual income</InputLabel>
+            <Select
+              id="select-basic"
+              type="income"
+              name="income"
+              value={this.state.income}
+              onChange={this.onChange}
+              required
+            >
+              <MenuItem value={0}>No Income</MenuItem>
+              <MenuItem value={1}>Less than $1000</MenuItem>
+              <MenuItem value={2}>$1000 to $2999</MenuItem>
+              <MenuItem value={3}>$3000 to $4999</MenuItem>
+              <MenuItem value={4}>$5000 to $6999</MenuItem>
+              <MenuItem value={5}>$7000 and above</MenuItem>
+            </Select>
+          </FormControl>
+          <br /><br />
           <Button
             className="button_style"
             variant="contained"
             color="primary"
             size="small"
-            disabled={this.state.username == ''}
+            disabled={this.state.username == '' && this.state.password == ''}
             onClick={this.register}
           >
             Register
