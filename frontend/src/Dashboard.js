0d776f362458db153a836bc455faa5416ff0d3db
Darryl Soh Soon Yong <ssy.darryl@gmail.com>
2022-08-28 03:41:28 +0800

    Complete final calculation for economy score

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -17,6 +17,7 @@ export default class Dashboard extends Component {
       questionnaireScore: '',
       wealth: '',
       registrationDate: '',
+      economyScore : '',
     };
   }
 
@@ -27,9 +28,86 @@ export default class Dashboard extends Component {
     } else {
       this.setState({ token: token }, () => {
         this.getUser();
+        this.calculateEconomyScore();
       });
     }
   }
+
+  getOverallRisk = () => {
+
+      let riskPercent = (this.state.income*5+this.state.questionnaireScore*2+this.state.wealth*5+this.getMonthDifference(new Date(this.state.registrationDate), new Date())+this.state.economyScore)
+      console.log(riskPercent)
+      if (riskPercent > 79){
+          return "You have a high tolerance for risk"
+      }
+      if (riskPercent > 59){
+          return "You have a moderately high tolerance for risk"
+      }
+      if (riskPercent > 39){
+          return "You have a moderate tolerance for risk"
+      }
+      if (riskPercent > 19){
+          return "You have a moderately low tolerance for risk"
+      }
+      else{
+          return "You have a low tolerance for risk"
+      }
+  }
+
+  calculateEconomyScore = () => {
+    this.setState({ loading: true});
+    let data = '?';
+    data = `${data}`;
+    axios.get(`https://tablebuilder.singstat.gov.sg/api/table/tabledata/M015631?limit=1&sortBy=key desc`)
+        .then((res) => {
+        let GDPChange = res.data.Data.row[0].columns[0].value;
+        let economyScore = 0;
+        if (GDPChange < -80){
+            economyScore = 0;
+        }
+        else if (GDPChange < -60){
+            economyScore = 1;
+        }
+        else if (GDPChange < -40){
+            economyScore = 2;
+        }
+        else if (GDPChange < -20){
+            economyScore = 3;
+        }
+        else if (GDPChange < -9){
+            economyScore = 4;
+        }
+        else if (GDPChange < 10){
+            economyScore = 5;
+        }
+        else if (GDPChange < 21){
+            economyScore = 6;
+        }
+        else if (GDPChange < 41){
+            economyScore = 7;
+        }
+        else if (GDPChange < 61){
+            economyScore = 8;
+        }
+        else if (GDPChange < 81){
+            economyScore = 9;
+        }
+        else{
+            economyScore = 10;
+        }
+        this.setState({economyScore: economyScore})
+
+        }).catch((err) => {
+          swal({
+            text: err.response.data.errorMessage,
+            icon: "error",
+            type: "error"
+          });
+          this.setState({loading: false})
+    });
+
+  }
+
 
   getUser = () => {
     
@@ -52,7 +130,7 @@ export default class Dashboard extends Component {
         icon: "error",
         type: "error"
       });
-      this.setState({ loading: false, products: []},()=>{});
+      this.setState({ loading: false},()=>{});
     });
   }
 
@@ -93,7 +171,8 @@ export default class Dashboard extends Component {
           Questionnaire Score: {this.state.questionnaireScore*2}/20 <br/>
           Wealth Score: {this.state.wealth*5}/25 <br/>
           Duration Score: {this.getMonthDifference(new Date(this.state.registrationDate), new Date())}/10 <br/>
-          <h1>Your risk level is {100-(this.state.income*5+this.state.questionnaireScore*2+this.state.wealth*5+this.getMonthDifference(new Date(this.state.registrationDate), new Date()))}%</h1>
+          Economy Score : {this.state.economyScore}/10<br/>
+            <h1>{this.getOverallRisk()}</h1>
           <Button
             className="button_style"
             variant="contained"
